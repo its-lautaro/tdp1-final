3e57cd330921ac53a4285820cba92397c29a1fcb
ignacioMonteagudo <ignaciomonteagudo98@gmail.com>
2023-10-04 21:09:25 -0300

    correciones

--- pseudo_codigo_main.c
+++ pseudo_codigo_main.c
@@ -6,11 +6,11 @@ Bucle del programa
 	Se lee el botón presionado
     
 	Si el estado primario es STAND_BY y se presionó algún botón
-		Avanzo al estado primario 'MENU'
+		Se avanza al estado primario 'MENU'
 
 
 	Si no se presionó ningún botón en 30 seg
-		Avanzo al estado primario 'STAND_BY'
+		Se avanza al estado primario 'STAND_BY'
 
 	Si el estado primario es 'MENU' 
 		Se presionó el botón UP
@@ -30,7 +30,7 @@ Bucle del programa
 			      		Decrementar la fecha
 			   	Sino
 			  		Se presionó el botón 'ENTER'
-			     			Avanzo al estado secundario 'MODIFICAR_HORA'
+			     			Se avanza al estado secundario 'MODIFICAR_HORA'
 
 		        
 		Si el estado secundario es 'MODIFICANDO_HORA'
@@ -41,8 +41,8 @@ Bucle del programa
 			      		Decrementar la hora
 			  	Sino
 			  		Se presionó el botón 'ENTER'
-			  			Avanzo al estado primario 'MENU'
-			  			Avanzo al estado secundario 'MODIFICANDO_FECHA'
+			  			Se avanza al estado primario 'MENU'
+			  			Se avanza al estado secundario 'MODIFICANDO_FECHA'
 		       	    
 
 	Si el estado primario es 'MODIFICAR_FECHA_HORA_PROGRAMA_RIEGO'
@@ -66,54 +66,54 @@ Bucle del programa
 						Decrementar la hora
 				  	Sino
 				  		Se presionó el botón 'ENTER'
-				 			Avanzo al estado secundario 'MODIFICAR_FECHA'
-							Avanzo al estado primario 'MENU'
+				 			Se avanza al estado secundario 'MODIFICAR_FECHA'
+							Se avanza al estado primario 'MENU'
 			
 			
 	Si el estado primario es 'SELECCIONAR_MODO'
     
 		Se presionó el botón 'UP' o 'DOWN'
 			Si el estado secundario es 'CAMBIAR_RIEGO_INTELIGENTE_OFF'
-				Avanzo al estado secundario 'RIEGO_INTELIGENTE_ON'
+				Se avanza al estado secundario 'RIEGO_INTELIGENTE_ON'
 	    		Sino
-	    	 		Avanzo al estado secundario 'RIEGO_INTELIGENTE_OFF'	
+	    	 		Se avanza al estado secundario 'RIEGO_INTELIGENTE_OFF'	
 	    		
 	    	Se presionó el botón 'ENTER'
 	    		Se cambia el modo inteligente dependiendo del estado secundario actual
 	    				    
 	  	Se presionó el botón 'CANCELAR'
-	  		Avanzo al estado primario 'MENU'
+	  		Se avanza al estado primario 'MENU'
 	    	
     	
    	Si el estado primario es 'REGAR'
 		Se presionó el botón 'CANCELAR'
-		  	Avanzo al estado secundario 'FINALIZAR'
+		  	Se avanza al estado secundario 'FINALIZAR'
 		
 		Si el estado secundario es 'PREPARADO'
-			Avanzo al estado secundario 'VÁLVULA1'
+			Se avanza al estado secundario 'VÁLVULA1'
 			Abrir válvula 1
 			Inicio contador de tiempo valvula 2
 
 		Si el estado secundario es 'VÁLVULA1'
 			Si se cumplió el tiempo de riego de la valvula 1
-				Avanzo al estado secundario 'VÁLVULA'
+				Se avanza al estado secundario 'VÁLVULA'
 				Cerrar válvula 1
 				Abrir válvula 2
 				Inicio contador de tiempo valvula 2
 
 		Si el estado secundario es 'VÁLVULA2'
 			Si se cumplió el tiempo de riego de la valvula 2
-				Avanzo al estado secundario 'PREPARADO'
-				Avanzo al estado primario 'MENU'
+				Se avanza al estado secundario 'PREPARADO'
+				Se avanza al estado primario 'MENU'
 				Cerrar válvula 2
 		      
 		Si el estado secundario es 'FINALIZAR'
 			Cerrar válvula 1
 			Cerrar válvula 2
-			Avanzo al estado secundario 'PREPARADO'
-			Avanzo al estado primario 'MENU'
+			Se avanza al estado secundario 'PREPARADO'
+			Se avanza al estado primario 'MENU'
 		
         
 	Si el estado primario es STAND_BY y es la hora de regar
-		Avanzo al estado primario 'REGAR'
-		Avanzo al estado secundario 'PREPARADO'
+		Se avanza al estado primario 'REGAR'
+		Se avanza al estado secundario 'PREPARADO'
