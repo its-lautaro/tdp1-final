3d53731d7a0e6ff8280255d31e7fa2a72a0f4d8a
ignacioMonteagudo <ignaciomonteagudo98@gmail.com>
2023-10-04 21:08:22 -0300

    correciones

--- pseudo_codigo_main.c
+++ pseudo_codigo_main.c
@@ -1,16 +1,11 @@
-
 Se avanza al estado primario STAND_BY
 Bucle del programa
 
 	Escribir en el lcd leyenda correspondiente según estado actual
 
-	Si el estado primario es STAND_BY y es la hora de regar
-		Avanzo al estado primario 'REGAR'
-		Avanzo al estado secundario 'PREPARADO'
-
 	Se lee el botón presionado
     
-	Si el estado primario es STAND_BY y se preionó algún botón
+	Si el estado primario es STAND_BY y se presionó algún botón
 		Avanzo al estado primario 'MENU'
 
 
@@ -18,80 +13,80 @@ Bucle del programa
 		Avanzo al estado primario 'STAND_BY'
 
 	Si el estado primario es 'MENU' 
-		Si presioné el botón UP
+		Se presionó el botón UP
 	    	    Avanzar estado secundario correspondiente
 		    
-		Si presioné el boton Enter
+		Se presionó el boton Enter
 		    Avanzar estado primario correspondiente
 
 
 	Si el estado primario es 'MODIFICAR_FECHA_HORA_ACTUAL'
 
 		Si el estado secundario es 'MODIFICANDO_FECHA'
-			Si presioné el botón 'UP'
+			Se presionó el botón 'UP'
 			      Incrementar la fecha
 			Sino   
-				Si presioné el botón 'DOWN'
+				Se presionó el botón 'DOWN'
 			      		Decrementar la fecha
 			   	Sino
-			  		Si presioné el botón 'ENTER'
+			  		Se presionó el botón 'ENTER'
 			     			Avanzo al estado secundario 'MODIFICAR_HORA'
 
 		        
 		Si el estado secundario es 'MODIFICANDO_HORA'
-			Si presioné el botón 'UP'
+			Se presionó el botón 'UP'
 			      Incrementar la hora
 			Sino   
-				Si presioné el botón 'DOWN'
+				Se presionó el botón 'DOWN'
 			      		Decrementar la hora
 			  	Sino
-			  		Si presioné el botón 'ENTER'
+			  		Se presionó el botón 'ENTER'
 			  			Avanzo al estado primario 'MENU'
 			  			Avanzo al estado secundario 'MODIFICANDO_FECHA'
 		       	    
 
 	Si el estado primario es 'MODIFICAR_FECHA_HORA_PROGRAMA_RIEGO'
 		Si el estado secundario es 'MODIFICANDO_FECHA'
-			Si presioné el botón 'UP'
+			Se presionó 'UP'
 				Incrementar día de la semana (lunes,martes,etc)
 			Sino
-				Si presioné el botón 'DOWN'
+				Se presionó el botón 'DOWN'
 			      		Decrementar  día de la semana (lunes,martes,etc)
 			      	Sino
-			      		Si presioné el botón 'ENTER'
+			      		Se presionó el botón 'ENTER'
 			   			estado_secundario = MODIFICAR_HORA
 				     
 			
 			
 		Si el estado secundario es 'MODIFICANDO_HORA)'
-				Si presioné el botón 'UP'
+				Se presionó el botón 'UP'
 					Incrementar la hora
 				Sino
-					Si presioné el botón 'DOWN'
+					Se presionó el botón 'DOWN'
 						Decrementar la hora
 				  	Sino
-				  		Si presioné el botón 'ENTER'
+				  		Se presionó el botón 'ENTER'
 				 			Avanzo al estado secundario 'MODIFICAR_FECHA'
 							Avanzo al estado primario 'MENU'
 			
 			
 	Si el estado primario es 'SELECCIONAR_MODO'
     
-		Si se presionó el botón 'UP' o 'DOWN'
+		Se presionó el botón 'UP' o 'DOWN'
 			Si el estado secundario es 'CAMBIAR_RIEGO_INTELIGENTE_OFF'
 				Avanzo al estado secundario 'RIEGO_INTELIGENTE_ON'
 	    		Sino
 	    	 		Avanzo al estado secundario 'RIEGO_INTELIGENTE_OFF'	
 	    		
-	    	Si presioné el botón 'ENTER'
+	    	Se presionó el botón 'ENTER'
 	    		Se cambia el modo inteligente dependiendo del estado secundario actual
 	    				    
-	  	Si se presionó el botón 'CANCELAR'
+	  	Se presionó el botón 'CANCELAR'
 	  		Avanzo al estado primario 'MENU'
 	    	
     	
    	Si el estado primario es 'REGAR'
-		Si se presionó el botón 'CANCELAR'
+		Se presionó el botón 'CANCELAR'
 		  	Avanzo al estado secundario 'FINALIZAR'
 		
 		Si el estado secundario es 'PREPARADO'
@@ -119,4 +114,6 @@ Bucle del programa
 			Avanzo al estado primario 'MENU'
 		
         
-         
+	Si el estado primario es STAND_BY y es la hora de regar
+		Avanzo al estado primario 'REGAR'
+		Avanzo al estado secundario 'PREPARADO'
